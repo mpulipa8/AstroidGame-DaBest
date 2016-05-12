55cc03bf011f8e3fd047df08f1b8d73c5e2cded4
Ojas <orade8@students.d125.org>
2016-05-12 10:47:55 -0500

    finished astroid class

--- AstroidGame-DaBest/AstroidGame-DaBest/Asteroid.java
+++ AstroidGame-DaBest/AstroidGame-DaBest/Asteroid.java
@@ -1,15 +1,53 @@
 import java.awt.*;
-public class Asteroid{
-    int Ax=0;
-    int Ay=0;
-    public Asteroid(Graphics g){
-        g.drawRect(0,0,20,20);x
+import java.awt.event.*;
+import javax.swing.*;
+import java.applet.Applet;
+public class Asteroid extends Applet implements ActionListener
+
+{
+    private int Ax=0;
+    private int Ay=0;
+    private int Xinc=1;
+    private int Yinc=1;
+
+    public Asteroid(Graphics g, int x, int y){
+        g.drawRect(Ax,Ay,20,20);
+        Xinc=x;
+        Yinc=y;
     }
-   
-    
-    
-    
-    
-    
-    
+
+    public void move()
+    {
+        if(collidesX() || collidesY()){
+            Xinc=0-Xinc;
+        }
+        Ax+=Xinc;
+        Ay+=Yinc;
+        repaint();
+    }
+
+    public void setX(int x){
+        Xinc=x;
+    }
+
+    public void setY(int y){
+        Yinc=y;
+    }
+
+    private boolean collidesX(){
+        if(Ax==0|| Ax==1000){
+            return true;
+        }
+        return false;
+    }
+    private boolean collidesY(){
+        if(Ay==0 || Ay==1000){
+            return true;
+        }
+        return false;
+    }
+
+    public void actionPerformed(ActionEvent e){
+        repaint();
+    }
 }
