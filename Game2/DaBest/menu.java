65e6b26a9634cb5d77f69d8e0fe11cd04bbc3fee
Maurya <mpulipa8@students.d125.org>
2016-05-17 11:02:28 -0500

    made menu

--- Game2/DaBest/menu.java
+++ Game2/DaBest/menu.java
@@ -9,25 +9,55 @@ import javax.swing.JPanel;
 
 public class menu extends JFrame implements ActionListener
 
-
 {
-    
     JPanel menu;
-     Button play; 
+    Button play; 
+
+    public void init()  
+    { 
+        setLayout(new FlowLayout()); 
+        play = new Button("Play!"); 
+        add(play);   
+        play.addActionListener(this); 
+    } 
+
     public menu()
     {
-        
-        setDegaultCloseOperation(EXIT_ON_CLOSE);
+        BufferedImage ss=null;
+        BufferedImage sss=null;
+
+        try{
+            ss=ImageIO.read(new File("aster.jpg"));
+        }catch(IOException e){
+        }
+        try{
+            sss=ImageIO.read(new File("stars_texture2959.jpg"));
+        }catch(IOException e){
+        }
+
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
         setSize(800,800);
         setVisible(true);
-        
-        menu = new JPanel();
+
+        menu1 = new JPanel();
         button = new JButton("button");
+
+        menu1.setSize(800,800);
+        menu1.setBackground(sss);
+        menu1.setVisiable(true);
+        menu1.add(button);
         
-        menu.setSize(800,800);
-        menu.setBackground(
         
+        button.setLocation(600,600);
+        button.addActionListener(this); 
+        
+        add(menu, BorderLayout.CENTER);
+
     }
-    
-    
+    public void actionPerformed(ActionEvent a){
+    JPanel panel = Game.Game();
+    this.remove(menu);
+    this.add(panel);   
+}
+
 }
